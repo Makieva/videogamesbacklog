be2769521a49e46f4f3e6c2a98cca2f353f37be6
Makieva <maeva.oukid@gmail.com>
2024-07-05 02:21:01 +0200

    wip page accueil

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
-// import "./Home.css";
+import "./Home.css";
+import CategButton from "./components/CategButton";
+import Cards from "./components/Cards";
 
 const Home = () => {
-    return <h1>Page d'Accueil</h1>
+    return (
+        <>
+        <section>
+            <h1>Page d'Accueil</h1>
+        </section>
+        <section className="sectionCards">
+            <CategButton></CategButton>
+            <Cards></Cards>
+        </section>
+        </>
+    )
 };
-
 export default Home;
---
+++ frontend/src/pages/home/components/CategButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CategButton = () => {
+  return <div>Categorie 1</div>;
+};
+
+export default CategButton;
