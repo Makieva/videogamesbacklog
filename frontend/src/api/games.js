50d1c5bfa8dac9adb2a67b140234ce44967b52a5
Makieva <maeva.oukid@gmail.com>
2024-07-07 13:21:30 +0200

    restructuration des composants gamelist et home + searchbar

--- frontend/src/api/games.js
+++ frontend/src/api/games.js
@@ -1,21 +1,6 @@
 import axios from "axios";
 import { Games } from "../models/Games";
 
-// export const getGamesList = async() => {
-//     const urlCall = `${process.env.REACT_APP_API_URL}/games?key=${process.env.REACT_APP_API_KEY}`;
-//     console.log('test');
-
-//     return axios
-//         .get <{results: Games[]}> (urlCall)
-//         .then((response) => {
-//             return response.data;
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-
-// }
-
 export const getGamesList = async () => {
   const urlCall = `${process.env.REACT_APP_API_URL}/games?key=${process.env.REACT_APP_API_KEY}`;
   console.log(urlCall);
@@ -71,3 +56,15 @@ export const getGamesFromGenreList = async (genreId) => {
     );
   }
 };
+
+export const getGamesBySearch = async (search) => {
+  const urlCall = `${process.env.REACT_APP_API_URL}/games?key=${process.env.REACT_APP_API_KEY}&search=${search}`;
+  try{
+    const response = await axios.get(urlCall);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la recherche:", error
+    );
+  }
+}
--- frontend/src/componentsShared/SearchBar.js
+++ frontend/src/componentsShared/SearchBar.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
-const SearchBar = () => {
-    return (
-        <p>this is a searchbar</p>
-    )
-}
+const SearchBar = ({ onSearch }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSearch = () => {
+        onSearch(inputValue);
+    };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="searchBar"
+        onChange={handleInputChange}
+        placeholder="Rechercher..."
+        value={inputValue}
+      ></input>
+      <button onClick={handleSearch}>Rechercher</button>
+    </div>
+  );
+};
 
 export default SearchBar;
